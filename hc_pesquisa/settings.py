21ce5ad19b0ad0428e2473c80b1301e58dcad189
Paulo Felix <Paulo.felixper@gmail.com>
2025-11-24 22:06:20 -0300

    Deploy
    Ajustes para o deploy

--- hc_pesquisa/settings.py
+++ hc_pesquisa/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4k4-_ph#%@4g4euhricdg)2*_2z=v$563*)e467@6mp@wx@4r*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
